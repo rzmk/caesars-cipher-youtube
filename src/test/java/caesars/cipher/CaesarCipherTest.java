aafb4c7997402868d78706fa21d4834950ef4953
rzmk <mueez25@gmail.com>
2021-09-03 21:15:19 -0400

    Initial commit

---
+++ src/test/java/caesars/cipher/CaesarCipherTest.java
@@ -0,0 +1,21 @@
+package caesars.cipher;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CaesarCipherTest {
+
+	private CaesarsCipher caesarCipher = new CaesarsCipher();
+	
+	@Test
+	void testCipheredMessageWithOffsetOf12() {
+		assertEquals("tai mdq kag pauzs fapmk", caesarCipher.cipher("how are you doing today", 12));
+	}
+
+	@Test
+	void testEmptyString() {
+		assertEquals("", caesarCipher.cipher("", 12));
+	}
+
+}
